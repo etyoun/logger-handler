e32271814de6b72a1d9209c1b28fad1a56e083d2
et <younet96.yet@gmail.com>
2024-02-29 10:21:08 +0900

    dt now to name logs file

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import json
 from logging import getLogger, config
+from datetime import datetime
 
 
-with open("./log_config.json", 'r') as f:
+# 環境設定ファイル(JSON)を読み込む
+with open("./setting/log_config.json", 'r') as f:
     log_conf = json.load(f)
 
+# ファイル名をタイムスタンプで作成
+log_conf["handlers"]["fileHandler"]["filename"] = \
+    './data/logs/{}.logs'.format(datetime.now().strftime("%Y%m%d%H%M%S"))
+
+# パラメータが設定されていればレベルをINFOからDEBUGに置換
+# if ARGS.verbose:
+#     log_conf["handlers"]["fileHandler"]["level"] = DEBUG
+
+# logging設定
 config.dictConfig(log_conf)
 
 logger = getLogger(__name__)
