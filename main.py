1efaa71ae6db89cc048b0330ec8e0be003eddc7d
et <younet96.yet@gmail.com>
2024-02-29 10:08:55 +0900

    test: ok

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-from logging import getLogger
+import json
+from logging import getLogger, config
 
+
+with open("./log_config.json", 'r') as f:
+    log_conf = json.load(f)
+
+config.dictConfig(log_conf)
 
 logger = getLogger(__name__)
-logger.info("Starting")
+logger.info('Process Start!')
+logger.debug('debug')
+logger.info('info')
+logger.warning('warning')
+logger.error('error')
+logger.info('Process End!')
